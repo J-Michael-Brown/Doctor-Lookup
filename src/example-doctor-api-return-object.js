129b7e02724367c81b927e88b70894334c4cd85f
John Michael Brown <jmichaelbrown132737@gmail.com>
2019-01-18 13:18:32 -0800

    create file with an example of a single doctor object as returned by API call

---
+++ src/example-doctor-api-return-object.js
@@ -0,0 +1,174 @@
+{
+   "practices": [
+       {
+           "location_slug": "or-hillsboro",
+           "within_search_area": true,
+           "distance": 5.7195380897899035,
+           "lat": 45.53928,
+           "lon": -122.87708,
+           "uid": "f8f491aa900d8734ec82fab364a53a7e",
+           "name": "Kaiser Permanente Westside Medical Center",
+           "website": "http://kp.org/kpweb/facilitydir/facility.do?id=302181",
+           "accepts_new_patients": true,
+           "insurance_uids": [
+               "humana-humanaatlantahmoxhix",
+               "premerabcbs-premeraheritagesignaturehix",
+               "premerabcbs-lifewiseconnecthix",
+               "medicaid-medicaid",
+               "medicare-medicare",
+               "aetna-aetnamdbronzesilverandgoldhmo"
+           ],
+           "visit_address": {
+               "city": "Hillsboro",
+               "lat": 45.53928,
+               "lon": -122.87708,
+               "state": "OR",
+               "state_long": "Oregon",
+               "street": "2875 NW Stucki Ave",
+               "zip": "97124"
+           },
+           "office_hours": [],
+           "phones": [
+               {
+                   "number": "9713101000",
+                   "type": "business_landline"
+               },
+               {
+                   "number": "9713101000",
+                   "type": "landline"
+               }
+           ],
+           "languages": [
+               {
+                   "name": "English",
+                   "code": "en"
+               }
+           ]
+       }
+   ],
+   "educations": [
+       {
+           "school": "Yale University School of Medicine",
+           "degree": "MD"
+       },
+       {
+           "school": "Ventura County Medical Center",
+           "degree": "Residency"
+       }
+   ],
+   "profile": {
+       "first_name": "Eric",
+       "middle_name": "M.",
+       "last_name": "Poolman",
+       "slug": "eric-poolman-md",
+       "title": "MD",
+       "image_url": "https://asset2.betterdoctor.com/images/558ceb2ff22866052a00113d-1_thumbnail.jpg",
+       "gender": "male",
+       "languages": [
+           {
+               "name": "English",
+               "code": "en"
+           }
+       ],
+       "bio": "Dr. Eric Poolman, MD MBA, specialist in family medicine, currently practices medicine at Portland, Oregon and Scappoose, Oregon.\n\nDr. Poolman is licensed to treat patients in Oregon.\n\nIn addition to having active medical licenses, Dr. Poolman has successfully passed a background check including a medical license verification (active) and screening for malpractice history (none found)."
+   },
+   "ratings": [],
+   "insurances": [
+       {
+           "insurance_plan": {
+               "uid": "humana-humanaatlantahmoxhix",
+               "name": "Atlanta - HMOx HIX",
+               "category": [
+                   "medical"
+               ]
+           },
+           "insurance_provider": {
+               "uid": "humana",
+               "name": "Humana"
+           }
+       },
+       {
+           "insurance_plan": {
+               "uid": "premerabcbs-premeraheritagesignaturehix",
+               "name": "Premera Heritage Signature - HIX",
+               "category": [
+                   "medical"
+               ]
+           },
+           "insurance_provider": {
+               "uid": "premerabluecrossblueshield",
+               "name": "BCBS"
+           }
+       },
+       {
+           "insurance_plan": {
+               "uid": "premerabcbs-lifewiseconnecthix",
+               "name": "LifeWise Connect - HIX",
+               "category": [
+                   "medical"
+               ]
+           },
+           "insurance_provider": {
+               "uid": "premerabluecrossblueshield",
+               "name": "BCBS"
+           }
+       },
+       {
+           "insurance_plan": {
+               "uid": "medicaid-medicaid",
+               "name": "Medicaid",
+               "category": [
+                   "medical"
+               ]
+           },
+           "insurance_provider": {
+               "uid": "medicaid",
+               "name": "Medicaid"
+           }
+       },
+       {
+           "insurance_plan": {
+               "uid": "medicare-medicare",
+               "name": "Medicare",
+               "category": [
+                   "medical"
+               ]
+           },
+           "insurance_provider": {
+               "uid": "medicare",
+               "name": "Medicare"
+           }
+       },
+       {
+           "insurance_plan": {
+               "uid": "aetna-aetnamdbronzesilverandgoldhmo",
+               "name": "MD Bronze Silver  Gold - HMO",
+               "category": [
+                   "medical"
+               ]
+           },
+           "insurance_provider": {
+               "uid": "aetna",
+               "name": "Aetna"
+           }
+       }
+   ],
+   "specialties": [
+       {
+           "uid": "family-practitioner",
+           "name": "Family Medicine",
+           "description": "Specializes in your and your family's total health.",
+           "category": "medical",
+           "actor": "Family Practitioner",
+           "actors": "Family Practitioners"
+       }
+   ],
+   "licenses": [
+       {
+           "number": "MD152762",
+           "state": "OR"
+       }
+   ],
+   "uid": "30e2531dd315a75e2ca0e9bca48bcc43",
+   "npi": "1508028226"
+}
