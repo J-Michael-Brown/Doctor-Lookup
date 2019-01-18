938b498acc9908a2a9403c7b8fcd091f06035d73
John Michael Brown <jmichaelbrown132737@gmail.com>
2019-01-18 12:45:42 -0800

    add logic to test for api call

--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import './styles.css';
 import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { doctors } from './database-search.js';
 
 $(document).ready(function() {
-
+  let localDoctors = doctors();
+  if (localDoctors) {
+    if(localDoctors.meta.error){
+      console.log(localDoctors.meta.message);
+    } else {
+      // let doctors = [];
+      localDoctors.data.forEach(function(doctor) {
+        // doctor.practices.forEach(function(practice) {
+          //
+          // });
+          console.log(`${doctor.profile.first_name} ${doctor.profile.last_name}, ${doctor.profile.title}`);
+          doctor.specialties.forEach(function(specialty) {
+            console.log(specialty.actor);
+          });
+        })
+        // return doctors;
+      }
+  } else {
+    console.log("API call fail.");
+  }
 
 });
