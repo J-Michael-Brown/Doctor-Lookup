a0f17d8ae032115a14aafeff8a2e6ff910c395d7
Student <example@example.com>
2019-01-18 16:49:32 -0800

    add spec to aquire list of doctors by query

--- spec/doctor-list-spec.js
+++ spec/doctor-list-spec.js
@@ -1,21 +1,21 @@
 import { DoctorList } from './../src/doctor-list.js';
+import { Meta } from './../src/meta.js';
 
 describe('DoctorList', function() {
   let testDoctors;
+  let meta;
   beforeEach(function() {
-    testDoctors = new DoctorList();
+    meta = new Meta('acne');
+    testDoctors = new DoctorList(meta);
   });
 
   describe('getDoctors()', function() {
-    it('should call the BetterDoctor API to generate a list of doctors within a given locations\' parameteres, and set the this.doctors array to this new list of doctors.', function() {
-      let getDoctorsTest = new DoctorList('badParams');
-      expect(getDoctorsTest.getDoctors('45.542863,-122.7944704,11').length).toEqual(10);
+    it('should call the BetterDoctor API to generate a list of doctors within a given query parametere, and set the this.doctors array to this new list of doctors.', function() {
+      let getDoctorsTest = new DoctorList();
+      expect(getDoctorsTest.doctors.length).toEqual(0);
+      getDoctorsTest.getDoctors(meta);
+      expect(getDoctorsTest.doctors.length)).toBeGreaterThan(0);
     });
   });
 
-  describe('specialty(actorSting)', function() {
-    it('should return a list of doctors matching a given actor.', function() {
-      expect(testDoctors.specialty('Family Practitioner')[0].specialties.actor).toEqual('Family Practitioner');
-    });
-  });
 });
