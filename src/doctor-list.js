6be6ba9289c6ad5fb0b0f99caf95a151cac69b4e
John Michael Brown <jmichaelbrown132737@gmail.com>
2019-01-27 13:42:39 -0800

    refactor getResponse to pass functions into parameter to execute asynce code

--- src/doctor-list.js
+++ src/doctor-list.js
@@ -1,41 +1,21 @@
 import $ from 'jquery';
-import { Doctor } from './doctor.js';
 
-  getResponse(meta) {
-    const doctors = [];
-
-    let promise = new Promise(function(success, failure) {
-      $.ajax({
-        url: meta.url,
-        type: 'GET',
-        data: mata.data,
-        success: function(response) {
-          return response;
-        },
-        error: function() {
-          console.log('api call failure');
-          apiCall = false;
-        }
-      });
-    });
-    promise.then(function(response) {
-      debugger;
-      return doctors;
-    });
-
-  }
-
-
-  //
-  // doctorsByName(name) {
-  //   const doctorsWithName = [];
-  //   doctors.forEach(function(doctor) {
-  //     if (doctor.profile.first_name.toLowerCase().includes(name) || doctor.profile.last_name.toLowerCase().includes(name) || doctor.profile.middle_name.toLowerCase().includes(name)) {
-  //       doctorsWithName.push(doctor);
-  //     }
-  //   });
-  //   return doctorsWithName;
-  // }
+function getResponse(meta, execute, error) {
+  let apiResponse;
+  $.ajax({
+    url: meta.url,
+    type: 'GET',
+    data: meta.data,
+    success: function(response) {
+      console.log('API call successful');
+      execute(response);
+    },
+    error: function() {
+      error();
+    }
+  });
+  return apiResponse;
+}
 
 
 export { getResponse };
