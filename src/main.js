7b64046224a318dea4a4b303a2461e0d91524351
John Michael Brown <jmichaelbrown132737@gmail.com>
2019-01-27 13:45:25 -0800

    succeed at first API call

--- src/main.js
+++ src/main.js
@@ -1,27 +1,17 @@
 import './styles.css';
 import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import { DoctorList } from './doctor-list.js';
+import { getResponse } from './doctor-list.js';
 import { Doctor } from './doctor.js';
 import { Meta } from './meta.js';
 
 $(document).ready(function() {
-  $.ajax({
-    url: 'https://api.betterdoctor.com/2016-03-01/conditions?',
-    type: 'GET',
-    data: {
-      user_key: process.env.exports.apiKey
-    },
-    success: function(response) {
-      response.data.forEach(function(condition) {
-        $('#conditions').append(`<option value="${condition.uid}">${condition.name}<option>`);
-      });
-      acceptFormSubmission();
-    },
-    error: function() {
+  function failCall() {
+    $('#error-message').text('API call failed');
+  }
+  let meta = new Meta({}, 'https://api.betterdoctor.com/2016-03-01/conditions?');
 
-    }
-  });
+  getResponse(meta, acceptForm, failCall);
 
   // function expandSpecialty() {
   //   $('.specialty-title').on('click', function(event) {
@@ -31,38 +21,31 @@ $(document).ready(function() {
   //   });
   // }
 
-  function acceptFormSubmission() {
+  function acceptForm(response) {
+    response.data.forEach(function(condition) {
+      $('#conditions').append(`<option value="${condition.uid}">${condition.name}<option>`);
+    });
     $('#query-form').submit(function(event) {
       event.preventDefault();
-      const query = $('#conditions option:selected').attr('value');
+      const userQuery = $('#conditions option:selected').attr('value');
       let doctors = [];
-      const meta = new Meta(query);
-      console.log(query);
-      $.ajax({
-        url: meta.url,
-        type: 'GET',
-        data: {
-          query: meta.query,
-          user_key: process.env.exports.apiKey
-        },
-        success: function(response) {
-          if(response.meta.error){
-            console.log(`${response.meta.message}`);
-          } else {
-            response.data.forEach(function(doctorSnippet) {
-              doctors.push(new Doctor(doctorSnippet));
-            });
-            displayDoctors(doctors);
-          }
+      const meta = new Meta({query: userQuery});
 
-        },
-        error: function() {
-          console.log('api call failure');
-        }
-      });
+      const response = getResponse(meta);
+      if(!response) {
+        $('#error-message').text('no response');
+      } else if (response.meta.error) {
+        $('#error-message').text(response.meta.message);
+      } else {
+        response.data.forEach(function(doctorSnippet) {
+          doctors.push(new Doctor(doctorSnippet));
+        });
+        displayDoctors(doctors);
+      }
     });
-
   }
+
+
 
   function displayDoctors(doctorList) {
     $('query-form').hide();
