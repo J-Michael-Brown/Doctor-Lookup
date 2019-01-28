c675c3b2fb51502233f3028c665cc97caf1a7e80
John Michael Brown <jmichaelbrown132737@gmail.com>
2019-01-27 16:17:04 -0800

    import build-site.js and use it's extra functionality

--- src/main.js
+++ src/main.js
@@ -4,12 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { getResponse } from './get-response.js';
 import { Doctor } from './doctor.js';
 import { Meta } from './meta.js';
+import { addStates, getMeta, addDoctors } from './build-site.js';
 
 $(document).ready(function() {
   function failCall() {
     $('#error-message').text('API call failed');
   }
   let meta = new Meta({}, 'https://api.betterdoctor.com/2016-03-01/conditions?');
+  addStates();
 
   getResponse(meta, acceptForm, failCall);
 
@@ -20,8 +22,7 @@ $(document).ready(function() {
     $('#query-form').submit(function(event) {
       event.preventDefault();
 
-      const userQuery = $('#conditions option:selected').attr('value');
-      const meta = new Meta({query: userQuery});
+      const meta = getMeta();
 
       getResponse(meta, displayDoctors, failCall);
     });
@@ -39,31 +40,17 @@ $(document).ready(function() {
         doctorList.push(new Doctor(doctorSnippet));
       });
 
-      $('query-form').hide();
-      doctorList.forEach(function(doctor, doctorIndex) {
-        $('#doctors').append(`<dt id="${doctorIndex}">${doctor.profile.first_name} ${doctor.profile.middle_name}. ${doctor.profile.last_name}</dt>`);
-        $('#doctors').append(`<dd class="extended-bio" id="${doctorIndex}-info"><dd>`);
-
-        $(`#${doctorIndex}-info`).append('<h5 class="locations-tag">Locations</h5>');
-        $(`#${doctorIndex}-info`).append(`<ul id="${doctorIndex}-locations">`);
-        doctor.practices.forEach(function(practice) {
-          if (practice.website) {
-            $(`#${doctorIndex}-locations`).append(`<li><a href="${practice.website}">${practice.visit_address.city} ${practice.visit_address.state}, ${practice.visit_address.street}</a></li>`);
-          } else {
-            $(`#${doctorIndex}-locations`).append(`<li>${practice.visit_address.city} ${practice.visit_address.state}, ${practice.visit_address.street}</li>`);
-          }
-        });
-        $(`#${doctorIndex}`).append('</ul>');
-
-        $(`#${doctorIndex}-info`).append(`<p>${doctor.profile.bio}</p>`);
-      });
-
-      $('dt').on('click', function(event) {
-        event.preventDefault();
-        const doctorID = $(this).attr('id');
-        $(`#${doctorID}-info`).slideToggle();
-      });
+      addDoctors(doctorList);
+      doctorListInteractions();
     }
+  }
+
+  function doctorListInteractions() {
+    $('dt').on('click', function(event) {
+      event.preventDefault();
+      const doctorID = $(this).attr('id');
+      $(`#${doctorID}-info`).slideToggle();
+    });
   }
 
 });
