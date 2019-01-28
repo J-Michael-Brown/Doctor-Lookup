1d07e60c2245b9512d89b5696f897b3965bd1663
John Michael Brown <jmichaelbrown132737@gmail.com>
2019-01-27 17:29:03 -0800

    refactor to include meta message in error message

--- src/main.js
+++ src/main.js
@@ -7,39 +7,43 @@ import { Meta } from './meta.js';
 import { addStates, getMeta, addDoctors } from './build-site.js';
 
 $(document).ready(function() {
-  function failCall() {
-    $('#error-message').text('API call failed');
+  function failCall(response) {
+    let errorMessage = 'API call failed';
+    if(response.responseJSON){
+      errorMessage = response.responseJSON.meta.message;
+    }
+    $('#error-message').text(errorMessage);
   }
+
   let meta = new Meta({}, 'https://api.betterdoctor.com/2016-03-01/conditions?');
   addStates();
 
   getResponse(meta, acceptForm, failCall);
 
   function acceptForm(response) {
-    response.data.forEach(function(condition) {
-      $('#conditions').append(`<option value="${condition.uid}">${condition.name}<option>`);
-    });
-    $('#query-form').submit(function(event) {
-      event.preventDefault();
-
-      const meta = getMeta();
-
-      getResponse(meta, displayDoctors, failCall);
-    });
+    if (response.meta.error) {
+      $('#error-message').text(response.meta.message);
+    } else {
+      response.data.forEach(function(condition) {
+        $('#conditions').append(`<option value="${condition.uid}">${condition.name}<option>`);
+      });
+      $('#query-form').submit(function(event) {
+        event.preventDefault();
+        const meta = getMeta();
+        getResponse(meta, displayDoctors, failCall);
+      });
+    }
   }
 
   function displayDoctors(response) {
     $('#doctors').empty();
-
     let doctorList = [];
-
     if (response.meta.error) {
       $('#error-message').text(response.meta.message);
     } else {
       response.data.forEach(function(doctorSnippet) {
         doctorList.push(new Doctor(doctorSnippet));
       });
-
       addDoctors(doctorList);
       doctorListInteractions();
     }
